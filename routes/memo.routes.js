c9fbd20bdeb25907fda4bd56b1c0143706574048
soft26wala <soft26wabdev@gmail.com>
2026-08-20 23:12:00 +0530

    v1.3.26

--- routes/memo.routes.js
+++ routes/memo.routes.js
@@ -5,7 +5,7 @@ const { verifyToken } = require('../middleware/auth.middleware');
 const { checkPermission } = require('../middleware/permission.middleware');
 
 router.get('/', verifyToken, checkPermission('Memo Management'), getMemos);
-router.post('/', verifyToken, checkPermission('Create'), createMemo);
-router.put('/:id/status', verifyToken, checkPermission('Edit'), updateMemoStatus);
+router.post('/', verifyToken, checkPermission('Memo Management'), createMemo);
+router.put('/:id/status', verifyToken, checkPermission('Memo Management'), updateMemoStatus);
 
 module.exports = router;
